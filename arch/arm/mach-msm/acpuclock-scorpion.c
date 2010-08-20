9fdb9c96c1e70a0abbc917a90d90951af3451217
Adam Green <greena88@gmail.com>
2010-08-20 22:52:34 +0100

    bravo: add 1267mhz and 1305mhz oc options

--- arch/arm/mach-msm/acpuclock-scorpion.c
+++ arch/arm/mach-msm/acpuclock-scorpion.c
@@ -95,6 +95,8 @@ struct clkctl_acpu_speed acpu_freq_tbl[] = {
 	{ 1036800, CCTL(CLK_TCXO, 1),		SRC_SCPLL, 0x1B, 0, 1275 },
 	{ 1075200, CCTL(CLK_TCXO, 1),		SRC_SCPLL, 0x1C, 0, 1275 },
 	{ 1113600, CCTL(CLK_TCXO, 1),		SRC_SCPLL, 0x1D, 0, 1275 },
+        { 1267200, CCTL(CLK_TCXO, 1),           SRC_SCPLL, 0x1E, 0, 1300 },
+        { 1305600, CCTL(CLK_TCXO, 1),           SRC_SCPLL, 0x1F, 0, 1300 },
 	{ 0 },
 };
 
